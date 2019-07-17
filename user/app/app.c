5af32e079f5f14d7a0519afaa5fd9ac2f8eb7c9b
1995-liu <15091586325@example.com>
2019-07-17 16:49:26 +0800

    保存当前有错误的代码，并做记录

--- user/app/app.c
+++ user/app/app.c
@@ -2,6 +2,16 @@
 int main(void)
 {
     mocbox_GPIO_LED_Init();
-    LED_GPS_RED_ON();
-    
+    //EXTI_PC2_Config(void);
+    LED_MJ_GRE_ON();
+    LED_MJ_RED_ON();
+
+    LED_KB_GRE_ON();
+    LED_KB_RED_ON();
+
+    LED_KY_GRE_ON();
+    LED_KY_RED_ON();
+
+    LED_KK_GRE_ON();
+    LED_KK_RED_ON();
 }
--- user/bsp/bsp_mocbox_hardware.c
+++ user/bsp/bsp_mocbox_hardware.c
@@ -2,6 +2,32 @@
 #include "bsp_mocbox_hardware.h"
 #undef __BSP_MOCBOX_HARDWARE_C__
 
+
+void EXTI_PC2_Config(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    EXTI_InitTypeDef EXTI_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+    
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOG,ENABLE);
+    GPIO_InitStructure.GPIO_Pin=GPIO_Pin_6;
+    GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IPU;
+    GPIO_Init(GPIOG,&GPIO_InitStructure);
+
+    EXTI_InitStructure.EXTI_Line=EXTI_Line2;
+    EXTI_InitStructure.EXTI_Mode=EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger=EXTI_Trigger_Falling;
+    EXTI_InitStructure.EXTI_LineCmd=ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+    NVIC_InitStructure.NVIC_IRQChannel=EXTI2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority=0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
 void mocbox_GPIO_LED_Init(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
